757f3737b8f551e6d832d4cc9ac37e7d1fc942f4
ericmelz <eric@emelz.com>
2023-09-14 16:09:49 -0700

    small modifications

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,13 +3,20 @@ defineProps({
   msg: {
     type: String,
     required: true
+  },
+  link: {
+    type: String,
+    required: true
   }
 })
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="green">
+      <a v-bind:href="link">{{ msg }}</a>
+<!--      {{ msg }}-->
+    </h1>
     <h3>
       You’ve successfully created a project with
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
